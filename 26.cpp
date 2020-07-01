e7bc8bd0faddef9fef8f22eac34f998e9afe7216
Ishpreet <ishpreetsingh2808@gmail.com>
2020-07-01 12:51:50 -0400

    Perfect Squares

--- 26.cpp
+++ 26.cpp
@@ -3,7 +3,6 @@
 Sum Root to Leaf Numbers
 ------------------------
 
-Solution
 Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
 
 An example is the root-to-leaf path 1->2->3 which represents the number 123.
